3006c20dd72f90e281adbdfa589888998b42a54d
David Campbell <dcampbell092@gmail.com>
2020-03-03 22:35:23 +0000

    style edit

--- frontend/src/components/sports/SportForm.js
+++ frontend/src/components/sports/SportForm.js
@@ -1,40 +1,51 @@
-import React from 'react'
+import React from "react"
 
 const SportForm = ({ data, handleChange, handleSubmit }) => {
-  return (
-    <div>
-      <h1>Sport Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Name"
-            name="name"
-            value={data.name}
-            required />
-        </div>
+	console.log(data)
+	return (
+		<section className='main-body'>
+			<div>
+				<h1 className='main-heading'>Sport Form</h1>
+				<div className='form-wrapper'>
+					<form onSubmit={handleSubmit}>
+						<div className='main-form-group'>
+							<input
+								className='main-form-field'
+								onChange={handleChange}
+								placeholder='Name'
+								name='name'
+								value={data.name}
+								required
+							/>
+							<label for='name' class='main-form-label'>
+								Name
+							</label>
+						</div>
 
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Image"
-            name="image"
-            value={data.image} />
-        </div>
-
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            name="events"
-            value={data.events} />
-        </div>
-
-        <div className="button-div">
-          <button className="button" type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  )
+						<div className='main-form-group'>
+							<input
+								className='main-form-field'
+								onChange={handleChange}
+								placeholder='Image'
+								name='image'
+								value={data.image}
+								required
+							/>
+							<label for='image' class='main-form-label'>
+								Image URL
+							</label>
+						</div>
+						<hr className='divider' />
+						<div className='button-div'>
+							<button className='button is-rounded' type='submit'>
+								Submit
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default SportForm
